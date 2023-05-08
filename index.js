3bca13a01d1e29626a3f19007637d90518a2147f
Roberto <robertogarcia287@gmail.com>
2023-05-08 14:24:43 -0500

    Roberto's event listening

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const mainText = document. getElementById('button');
+    return  mainText.addEventListener('click', function() {
+       return alert('Stop it...');
+    })
 }
